5f1880df7e1ed1f5d465f858937a93504998160b
micjef <michaeljeffrysetiawan@gmail.com>
2024-11-21 07:30:25 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('Dinkes Sorong Selatan')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
